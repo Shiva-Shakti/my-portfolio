797f2fd966d03c625283a2f9ff5c236099189ea9
Shiva <shivashakti952@gmail.com>
2025-03-24 14:59:16 +0530

    Fixed Netlify build issue and updated Navbar links

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,9 +6,9 @@ function Navbar() {
     <nav className="navbar">
       <h2>My Portfolio</h2>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Projects</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="https://example.com">Valid Link</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
